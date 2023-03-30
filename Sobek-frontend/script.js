09a2c775b1ed846916d92f1800ef26badb30c93f
keshavbhateja <64486698+keshavbhateja@users.noreply.github.com>
2023-03-30 11:06:42 -0400

    Create script.js

---
+++ Sobek-frontend/script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Check if username and password are correct
+  if (username === "soliman" && password === "soliman") {
+    // Redirect to index.html
+    window.location.href = "DriverRating.html";
+  } else {
+    alert("Incorrect username or password.");
+  }
+});
