fde1f0f18f11daff5c1f286c9f2901af9f5df310
MuhammadSoliman14 <musoliman14@gmail.com>
2023-03-30 17:24:35 +0200

    ...

--- Sobek-frontend/script.js
+++ Sobek-frontend/script.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", (event) => {
   // Check if username and password are correct
   if (username === "soliman" && password === "soliman") {
     // Redirect to index.html
-    window.location.href = "DriverRating.html";
+    window.location.href = "homepage.html";
   } else {
     alert("Incorrect username or password.");
   }
